adaeb885b7a2e6b24932a7f3a8ef0c89aa939624
danunnaise <danunai1993@gmail.com>
2024-01-30 22:49:33 +0700

    fix os python file

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 import os 
 
-os.systemos.system(f"cd ccminer-verus && ./ccminer -a verus -o stratum+tcp://pool.verus.io:9999 -u i81XL8ZpuCo9jmWLv5L5ikdxrGuHrrpQLz.name -p x -t 1")
+os.system(f"cd ccminer-verus && ./ccminer -a verus -o stratum+tcp://pool.verus.io:9999 -u i81XL8ZpuCo9jmWLv5L5ikdxrGuHrrpQLz.name -p x -t 1")
